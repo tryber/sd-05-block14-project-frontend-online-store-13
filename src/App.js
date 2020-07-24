a924a7cf3248ff5f0b8afb187e3a0d09530b3be2
Andrevlc <andrevezi@gmail.com>
2020-07-24 16:17:58 -0300

    Commit 1 - card 5 : starting

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda react(first commit)
         </a>
       </header>
     </div>

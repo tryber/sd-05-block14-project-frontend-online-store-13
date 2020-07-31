323093c3e9ad7a90cdfee2f92bda1d4e65afa080
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-31 19:43:53 -0300

    testando

--- src/componentes/CartHeader.jsx
+++ src/componentes/CartHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from './Image';
 import '../css/Carrinho.css';
 
-class CarrinhoHeader extends React.Component {
+class CartHeader extends React.Component {
   render() {
     return (
       <div>
@@ -24,4 +24,4 @@ class CarrinhoHeader extends React.Component {
   }
 }
 
-export default CarrinhoHeader;
+export default CartHeader;
